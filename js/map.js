2dba9e9f79647819936e386848921f9146465689
slimwang <netbot@vip.qq.com>
2017-03-30 22:07:26 +0800

    remove the map codes in the html file to the map.js file

---
+++ js/map.js
@@ -0,0 +1,34 @@
+var map;
+var markers = [];
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 39.084287,
+            lng: 117.201238
+        },
+        zoom: 11
+    });
+    // add markers list
+    universities.forEach(function(university) {
+        var marker = new google.maps.Marker({
+            position: university.position,
+            map: map,
+            title: university.title
+        });
+        markers.push(marker);
+    });
+    // markers animate
+    markers.forEach(function(m) {
+        m.addListener('click', toggleBounce);
+
+        function toggleBounce() {
+            if (m.getAnimation() !== null) {
+                m.setAnimation(null);
+            } else {
+                m.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        }
+    });
+
+}
